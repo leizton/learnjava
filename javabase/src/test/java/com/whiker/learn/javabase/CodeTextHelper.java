d50a46bc08fe414f672e86e9ee5be5dc55e114ff
leizton <leizton@126.com>
2017-12-15 21:20:13 +0800

    CodeTextHelper

---
+++ javabase/src/test/java/com/whiker/learn/javabase/CodeTextHelper.java
@@ -0,0 +1,43 @@
+package com.whiker.learn.javabase;
+
+/**
+ * @author yiqun.fan create on 17-12-15.
+ */
+public class CodeTextHelper {
+
+    private interface Source {
+        boolean hasNext();
+
+        String next();
+    }
+
+    private interface Sink {
+        void receive(String s);
+
+        void complete();
+    }
+
+    private static final class FileLineSource implements Source {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public String next() {
+            return null;
+        }
+    }
+
+    private static final class FileSink implements Sink {
+
+        @Override
+        public void receive(String s) {
+        }
+
+        @Override
+        public void complete() {
+        }
+    }
+}
